4c01699fb81d63c360c35fb7b68221f3e0db1f35
User <User@DESKTOP-HK12S8N>
2024-12-03 19:01:37 +0300

    develop commit

---
+++ UI.ui.py
@@ -0,0 +1,1 @@
+print('https://i.pinimg.com/originals/39/44/ce/3944ce5506101d22641fa74e676a0021.jpg')
--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class Suprematism(QWidget):
             self.qp.drawPolygon(coords)
 
     def initUI(self):
-        self.setGeometry(300, 300, 1000, 1000)
+        self.setGeometry(600, 700, 500, 500)
         self.setWindowTitle('Рисование')
 
     def mousePressEvent(self, event):
@@ -60,7 +60,7 @@ class Suprematism(QWidget):
         if (event.button() == Qt.LeftButton):
             self.status = 1
         elif (event.button() == Qt.RightButton):
-            self.status = 2
+            self.status = 3
         self.drawf()
 
     def mouseMoveEvent(self, event):
